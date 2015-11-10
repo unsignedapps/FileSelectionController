9f666a6c373f81458df99d487bcb5478a3303999
Rob Amos <bok@unsignedapps.com>
2015-11-10 14:52:40 +1100

    Initial Commit

---
+++ FileSelectionController/FileSelectionController.h
@@ -0,0 +1,19 @@
+//
+//  FileSelectionController.h
+//  FileSelectionController
+//
+//  Created by Robert Amos on 10/11/2015.
+//  Copyright © 2015 Unsigned Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FileSelectionController.
+FOUNDATION_EXPORT double FileSelectionControllerVersionNumber;
+
+//! Project version string for FileSelectionController.
+FOUNDATION_EXPORT const unsigned char FileSelectionControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FileSelectionController/PublicHeader.h>
+
+
